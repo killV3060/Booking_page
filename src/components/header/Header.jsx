c6808bc010340bc3d32a5a60903ab159aed90a87
killV3060 <hqviet3060@gmail.com>
2025-08-08 23:15:28 +0700

    updated second

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,40 @@
+import "./header.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBed } from '@fortawesome/free-solid-svg-icons';
+import { faPlane } from '@fortawesome/free-solid-svg-icons';
+import { faCar } from '@fortawesome/free-solid-svg-icons';
+import { faTaxi } from '@fortawesome/free-solid-svg-icons';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerContainer">
+                <div className="headerList">
+                    <div className="headerListItem active">
+                        <div className="headerListItem"> </div>
+                            <FontAwesomeIcon icon={faBed} />
+                            <span>Stays</span>
+                        </div>
+                        <div className="headerListItem">
+                            <FontAwesomeIcon icon={faPlane} />
+                            <span>Flights</span>
+                        </div>
+                        <div className="headerListItem">
+                            <FontAwesomeIcon icon={faCar} />
+                            <span>Car rentals</span>
+                        </div>
+                        <div className="headerListItem">
+                            <FontAwesomeIcon icon={faBed} />
+                            <span>Attractions</span>
+                        </div>
+                        <div className="headerListItem">
+                            <FontAwesomeIcon icon={faTaxi} />
+                            <span>Airport taxis</span>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
